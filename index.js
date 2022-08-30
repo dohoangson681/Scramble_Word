300e04754c2013ec88eb8c82f4a6a3892c76f359
Son <dohoangson060801@gmail.com>
2022-08-30 10:10:57 +0700

    UI

---
+++ index.js
@@ -0,0 +1,7 @@
+const initGame = ()=>{
+    let randomObject = word[Math.floor(Math.random()*word.length)] ; 
+    let wordArray = randomObject.word.split("") ; 
+    // console.log(randomObject) ; 
+    // console.log(wordArray) ; 
+}
+initGame() ; 
